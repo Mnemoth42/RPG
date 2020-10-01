65bc3e03096b3bf419d64fcae33199b4c037094e
Samuel Pattuzzi <sam@pattuzzi.it>
2020-10-01 16:40:35 +0100

    2 Dialogue Architecture

---
+++ RPG Project/Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] Dialogue activeDialogue;
+
+        public string GetText()
+        {
+            if (activeDialogue == null)
+            {
+                 return "";
+            }
+
+            return activeDialogue.GetRootNode().GetText();
+        }
+    }
+
+}
